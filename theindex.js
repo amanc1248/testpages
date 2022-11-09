ecace061a54f66e17a2e8e3f8e2e9dce2d14b8f9
Aman Chaudhary <63035362+amanc1248@users.noreply.github.com>
2022-11-09 20:03:35 +0545

    Update theindex.js

--- theindex.js
+++ theindex.js
@@ -1,9 +1,11 @@
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
-console.log(" we are coming");
+class HelloWorldComponent extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    this.innerHTML = `<h1>Hello world!</h1>`;
+  }
+}
+
+customElements.define("hello-world", HelloWorldComponent);
