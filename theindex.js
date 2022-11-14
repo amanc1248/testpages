6675578c0fa4eedec15c2a156e3e8c4dc18f06ce
Aman Chaudhary <63035362+amanc1248@users.noreply.github.com>
2022-11-14 15:07:50 +0545

    Update theindex.js

--- theindex.js
+++ theindex.js
@@ -1,92 +1,197 @@
-
 console.log(window.location.href);
+
 const template = document.createElement("template");
 // template.innerHTML = ;
 
 class QuotesRates extends HTMLElement {
   constructor() {
+    
     super();
     this.showInfo = true;
     this.attachShadow({ mode: "open" });
     this.shadowRoot.appendChild(template.content.cloneNode(true));
-    this.createUpperBar();
-
   }
 
   connectedCallback() {
-    this.getQuotes();
+    this.createUpperBar();
+    this.getQuoteTotalCounts({ modalName: "LoadQuote" });
+    this.getQuotes({ pageNumber: 1 });
     // this.getRates();
-    document.getElementById("quotesRatesSelect").addEventListener('change',(event)=>{
+
+    document
+    .getElementById("quotesRatesSelect")
+    .addEventListener("change", (event) => {
       // console.log(event)
       const theValue = document.getElementById("quotesRatesSelect").value;
-      console.log("selectedvalue: ", theValue)
-      if(theValue=='quotes'){
-        document.getElementById('rates__div__id').remove();
-        this.getQuotes();
+      console.log("selectedvalue: ", theValue);
+      if (theValue == "quotes") {
+        document.getElementById("rates__div__id").remove();
+        // document.getElementById('pagination__id').remove();
+        this.getQuoteTotalCounts({ modalName: "LoadQuote" });
+        this.getQuotes({ pageNumber: 1 });
       }
-      if(theValue=='rates'){
-        document.getElementById('quotes__div__id').remove();
-        this.getRates();
+      if (theValue == "rates") {
+        document.getElementById("quotes__div__id").remove();
+        // document.getElementById('pagination__id').remove();
+        this.getQuoteTotalCounts({ modalName: "LoadPricingSettings" });
+        this.getRates({ pageNumber: 1 });
       }
     })
+
+    document
+    .getElementById("pagination__id")
+    .addEventListener("change", (e) => {
+      const theValue = document.getElementById("pagination__id").value;
+      const quotesOrRates =
+        document.getElementById("quotesRatesSelect").value;
+      if (quotesOrRates == "quotes") {
+        document.getElementById("quotes__div__id").remove();
+        this.getQuotes({ pageNumber: theValue });
+      }
+      if (quotesOrRates == "rates") {
+        document.getElementById("rates__div__id").remove();
+        this.getRates({ pageNumber: theValue });
+      }
+    });
   }
 
   // create upper bar
-  createUpperBar(){
+  createUpperBar() {
+    const quotesRatesHeight = this.getAttribute("quotesRatesHeight");
+    const quotesRatesWidth = this.getAttribute('quotesRatesWidth');
+    console.log(quotesRatesHeight, quotesRatesWidth)
     const para = document.createElement("div");
-para.innerHTML = `
-<select name="quotesRatesSelect" id="quotesRatesSelect">
-    <option value="quotes">Quotes</option>
-    <option value="rates">Rates</option>
-</select>
+    para.innerHTML = `
+<style>
+    #main__div{
+      width:${quotesRatesWidth};
+      height:${quotesRatesHeight};
+    }    
+    #upperBar{
+      display: flex;
+      justify-content: space-between; 
+    }
+    .pagination__div{
+      display:flex;
+    }
+    #quotes__div__id, #rates__div__id{
+      box-shadow: 0 15px 35px rgb(20 28 52 / 20%);
+      overflow:scroll;
+      height:${quotesRatesHeight}
+
+    }
+    
+    td{
+      background-color: #fff;
+    }
+    // .data__mapped{
+    //   background-color:  #2e4d763d;
+    // }
+    
+</style>
+    <div id="main__div" >
+      <div id="upperBar">
+        <select name="quotesRatesSelect" id="quotesRatesSelect" >
+          <option value="quotes">Quotes</option>
+          <option value="rates">Rates</option>
+        </select>
+        <div class="pagination__div">
+          <div class='quotes__rates__page_number__text'>Page Number</div>
+          <select name="pagination__pageNumber" id="pagination__id"></select>
+          </select>
+        </div>
+      </div>
+    </div>
 `;
-document.body.appendChild(para);
+    document.body.appendChild(para);
   }
 
   // 1. rendering some data
-  getQuotes() {
+  getQuotes({ pageNumber }) {
+    console.log(this)
+    const theId = this.getAttribute("quotesRateId");
+    console.log(theId);
     return new Promise((res, rej) => {
-      fetch("http://localhost:5000/tms/quotesRates?pageNumber=1&nPerPage=20&modelName=LoadQuote")
+      fetch(
+        `http://localhost:5000/tms/quotesRates?pageNumber=${pageNumber}&nPerPage=20&modelName=LoadQuote&token=${theId}`
+      )
         .then((data) => data.json())
         .then((json) => {
           console.log(json);
-          this.renderQuotes(json);
-          res();
+          if (json.message == "Not authorized") {
+            document.getElementById("upperBar").remove();
+          } else {
+            this.renderQuotes(json);
+            res();
+          }
+          // this.createUpperBar()
         })
         .catch((error) => rej(error));
     });
   }
 
   // 2. getting rates
-  getRates(){
+  getRates({ pageNumber }) {
+    const theId = this.getAttribute("quotesRateId");
+    console.log(theId);
     return new Promise((res, rej) => {
-      fetch("http://localhost:5000/tms/quotesRates?pageNumber=1&nPerPage=20&modelName=LoadPricingSettings")
+      fetch(
+        `http://localhost:5000/tms/quotesRates?pageNumber=${pageNumber}&nPerPage=20&modelName=LoadPricingSettings&token=${theId}`
+      )
         .then((data) => data.json())
         .then((json) => {
-          console.log(json);
-          this.renderRates(json);
-          res();
+          if (json.message == "Not authorized") {
+            document.getElementById("upperBar").remove();
+          } else {
+            this.renderRates(json);
+            res();
+          }
         })
         .catch((error) => rej(error));
     });
   }
 
   // 3. onChanging qutoes or rates
-  changeQuotesRates(){
+  changeQuotesRates() {
     var x = document.getElementById("quotesRatesSelect").value;
     // document.getElementById('quotesRatesSelect').addEventListener('')
-    console.log(x)
+    console.log(x);
   }
 
+  // 4. getQuotesCount
+  getQuoteTotalCounts({ modalName }) {
+    const theId = this.getAttribute("quotesRateId");
+    console.log(theId);
+    return new Promise((res, rej) => {
+      fetch(
+        `http://localhost:5000/tms/quotesRatesCount?modelName=${modalName}&token=${theId}`
+      )
+        .then((data) => data.json())
+        .then((json) => {
+          console.log(json);
+          if (json.message == "Not authorized") {
+            document.getElementById("upperBar").remove();
+          } else {
+            const { quotesCount } = json;
+            const totalPages = Math.ceil(quotesCount / 20);
+            console.log(totalPages);
+            this.renderPagination(totalPages);
+
+            res();
+          }
+        })
+        .catch((error) => rej(error));
+    });
+  }
 
   renderQuotes(data) {
-    console.log("i am rendering quotes")
+    console.log("i am rendering quotes");
 
     const para = document.createElement("div");
+    para.id='quotes__div__id';
     para.innerHTML = `
-    <div class="quotes__div" id="quotes__div__id">
-    <h1>Quotes</h1>
-        <table className="table table-card">
+    <div class="quotes__div">
+    <table class="table table-card">
       <thead>
         <tr>
           ${[
@@ -103,36 +208,41 @@ document.body.appendChild(para);
             "Base Price ($)",
             "Xtra Price",
             "Total Price",
-          ].map(
-            (th) =>
-              `<th
+          ]
+            .map(
+              (th) =>
+                `<th
               
               scope="col"
               className='text-center'
             >
               ${th}
             </th>`
-          )}
+            )
+            .join("")}
         </tr>
       </thead>
       <tbody>
-      ${data.map((d) => {
-        const basePriceObj = d.pricing.find((obj) => {
-          return obj.name === "Base Price";
-        });
-        const basePrice = basePriceObj ? parseFloat(basePriceObj.finalAmount) : 0;
-        let extraPrice = 0;
-        d.pricing.forEach((obj) => {
-          if (obj.name !== "Base Price")
-            extraPrice = extraPrice + parseFloat(obj.finalAmount);
-        });
-        return `<tr key=${d._id}>
+      ${data
+        .map((d) => {
+          const basePriceObj = d.pricing.find((obj) => {
+            return obj.name === "Base Price";
+          });
+          const basePrice = basePriceObj
+            ? parseFloat(basePriceObj.finalAmount)
+            : 0;
+          let extraPrice = 0;
+          d.pricing.forEach((obj) => {
+            if (obj.name !== "Base Price")
+              extraPrice = extraPrice + parseFloat(obj.finalAmount);
+          });
+          return `<tr key=${d._id} class="data__mapped">
               <td width="50">
-                {d.active ? (
-                  <span class=" badge badge-soft-green">Active</span>
-                ) : (
-                  <span class=" badge badge-gray-100">Expired</span>
-                )}
+                ${
+                  d.active
+                    ? `<span class=" badge badge-soft-green">Active</span>`
+                    : `<span class=" badge badge-gray-100">Expired</span>`
+                }
               </td>
               <td>${d.caller && d.caller.company_name}</td>
               <td>
@@ -164,46 +274,52 @@ document.body.appendChild(para);
               </td>
               
             </tr>`;
-      })}
+        })
+        .join("")}
     </tbody>
     </table>
     </div>`;
-    document.body.appendChild(para);
+    // document.body.appendChild(para);
+    document.getElementById('main__div').appendChild(para)
   }
 
-  renderRates(data){
-    console.log("i am rendering rates")
-    const ratesDiv = document.createElement('div');
+  renderRates(data) {
+    console.log("i am rendering rates");
+    const ratesDiv = document.createElement("div");
+    ratesDiv.id='rates__div__id'
     ratesDiv.innerHTML = `
-    <div class="rates__div" id="rates__div__id">
-    <h1>Rates</h1>
-            <table className="table table-card">
+    <div class="rates__div">
+            <table class="table table-card">
               <thead>
                 <tr>
-                  ${["status",
+                  ${[
+                    "status",
                     "Customer",
                     "Port / Shipper",
                     "Consignee",
                     "Zip Code",
-                    "City", 
-                    "State", 
+                    "City",
+                    "State",
                     "Base Price ($)",
                     "Extra Prices",
-                    "Total Prices"
-                  ].map((th) => (
-                    `<th
+                    "Total Prices",
+                  ]
+                    .map(
+                      (th) =>
+                        `<th
                       
                       scope="col"
                       className='text-center'
                     >
                       ${th}
                     </th>`
-                  ))}
+                    )
+                    .join("")}
                 </tr>
               </thead>
               <tbody>
-                ${ 
-                  data.map((d) => {
+                ${data
+                  .map((d) => {
                     const basePriceObj = d.pricing.find((obj) => {
                       return obj.name === "Base Price";
                     });
@@ -215,14 +331,13 @@ document.body.appendChild(para);
                       if (obj.name !== "Base Price")
                         extraPrice = extraPrice + parseFloat(obj.finalAmount);
                     });
-                    return (
-                      `<tr key={d._id}>
+                    return `<tr key={d._id} class="data__mapped">
                         <td width="50">
-                          ${d.active ? (
-                            `<span class=" badge badge-soft-green">Active</span>`
-                          ) : (
-                            `<span class=" badge badge-gray-100">Expired</span>`
-                          )}
+                          ${
+                            d.active
+                              ? `<span class=" badge badge-soft-green">Active</span>`
+                              : `<span class=" badge badge-gray-100">Expired</span>`
+                          }
                         </td>
                         <td>${d.caller && d.caller.company_name}</td>
                         <td>
@@ -232,23 +347,51 @@ document.body.appendChild(para);
                             })
                             .join(", ")}
                         </td>
-                        <td>${d.consignee.map((e, i) => { return e.company_name }).join(', ')}</td>
+                        <td>${d.consignee
+                          .map((e, i) => {
+                            return e.company_name;
+                          })
+                          .join(", ")}</td>
                         <td>${d.zipCode}</td>
                         <td>${d.city}</td>
                         <td>${d.state}</td>
-                        <td><div class="font-12">${basePrice.toFixed(2)}</div></td>
-                        <td><div class="font-12">${extraPrice && extraPrice.toFixed(2)}</div></td>
-                        <td><div class="font-12"> ${(basePrice + extraPrice).toFixed(2)}</div></td>
+                        <td><div class="font-12">${basePrice.toFixed(
+                          2
+                        )}</div></td>
+                        <td><div class="font-12">${
+                          extraPrice && extraPrice.toFixed(2)
+                        }</div></td>
+                        <td><div class="font-12"> ${(
+                          basePrice + extraPrice
+                        ).toFixed(2)}</div></td>
                         
                         
-                      </tr>`
-                    );
-                  })}
+                      </tr>`;
+                  })
+                  .join("")}
               </tbody>
             </table>
         </div>
     `;
-    document.body.appendChild(ratesDiv);
+    document.getElementById('main__div').appendChild(ratesDiv)
   }
+
+  renderPagination(totalPages) {
+    const selectElement = document.getElementById("pagination__id");
+    var child = selectElement.lastElementChild;
+    while (child) {
+      selectElement.removeChild(child);
+      child = selectElement.lastElementChild;
+    }
+    for (let index = 1; index <= totalPages; index++) {
+      const selectOption = document.createElement("option");
+      selectOption.label = index;
+      selectOption.value = index;
+      selectElement.appendChild(selectOption);
+    }
+    // document.body.appendChild(para);
+  }
+
 }
+
 window.customElements.define("quotes-rates", QuotesRates);
